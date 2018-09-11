fdc12a1454e6e53969465fc739a5b49c79b05dc0
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-09-10 17:20:15 -0700

    This program determines the biggest number entered by the user

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,18 +4,39 @@
 int main()
 {
   //declare variables here
+  int num1;
+  int num2;
+  int biggest;
+  biggest = num1;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
+  std::cin >> num1;
 
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
+  std::cin >> num2;
 
   //determine if first or second is bigger and store that in a third variable
+ 
+  //std::cout<< "Of those two numbers, the biggest is: ";
+  if (num1 < num2)
+  {
+    biggest = num2;
+  }
+  else if (num1 > num2)
+  {
+    biggest = num1;
+  }
+  else 
+  {
+    biggest = num1;
+  }
 
+  std::cout << "Of those two numbers, the biggest is: " << biggest << std::endl;
 
-  std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
+
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
